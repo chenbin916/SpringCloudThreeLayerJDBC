c61839f81058a9df3a66baefddbf63f68ce5968d
chenbin916 <40135830+chenbin916@users.noreply.github.com>
2019-04-24 17:06:02 +0800

    Update BaseDao.java
    添加batchInsert方法20190424

--- testThreeLayerJDBC/src/main/java/com/test/dao/BaseDao.java
+++ testThreeLayerJDBC/src/main/java/com/test/dao/BaseDao.java
@@ -16,6 +16,36 @@ public class BaseDao {
 	private JdbcTemplate jdbcTemplate;
 	
 	
+	public void batchInsert(String sql ,List<Person> persons) {
+		
+		jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
+			
+			@Override
+			public void setValues(PreparedStatement ps, int i) throws SQLException {
+				//	"(userType, userID, imageSequence, imagePathIP, imageURL, faceModelValue, updateTime, cmdFlag, cardGUID,personName,personSex,faceModelZone) " +
+				Person person = persons.get(i);
+				ps.setInt(1, person.getUserType());
+				ps.setString(2, person.getUserID());
+				ps.setString(3, person.getImageSequence());
+				ps.setString(4, person.getImagePathIP());
+				ps.setString(5, person.getImageURL());
+				ps.setString(6, person.getFaceModelValue());
+				ps.setDate(7, (Date) person.getUpdateTime());
+				ps.setString(8, person.getCmdFlag());
+				ps.setString(9, person.getCardGUID());
+				ps.setString(10, person.getPersonName());
+				ps.setString(11, person.getPersonSex());
+				ps.setString(12, person.getFaceModelZone());
+			}
+					
+			@Override
+			public int getBatchSize() {
+				return persons.size();
+			}
+		  });
+		
+	
+	}
 	
 	public int insert(String sql ,String userName) {
 
